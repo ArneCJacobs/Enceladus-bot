1af84c835d72861118c703a2362a8f4fdd4ab4d7
Arne Jacobs <arne.carla.jacobs@outlook.com>
2022-12-17 18:15:02 +0100

    fix: fixed predicion

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use std::{io::{self, BufRead}, time::{Instant, Duration}, collections::BTreeMap};
+use std::{io::{self, BufRead}, time::Instant, collections::BTreeMap};
 
 use bit_set::BitSet;
 use itertools::Itertools;
+use prettytable::{Table, Cell, Row};
 use serde::{Deserialize, Serialize};
 
 
@@ -76,7 +77,8 @@ struct State {
     current_state: Input,
     saved_expeditions: BitSet, 
     planet_map: BTreeMap<PlanetName, usize>,
-    turn: u64,
+    planet_names: Vec<String>,
+    turn: i64,
 }
 
 impl State {
@@ -84,100 +86,128 @@ impl State {
         self.turn += 1;
     }
 
-    fn get_state_cell(&mut self, planet_name: &PlanetName, turns_ahead: u64) -> &mut StateCell {
+    fn get_state_cell(&mut self, planet_name: &PlanetName, turns_ahead: i64) -> &mut StateCell {
         let planet_index = self.planet_map.get(planet_name).unwrap();
         // TODO: check if correct or if it is off by one 
-        let turn_index = self.turn + turns_ahead;
+        let turn_index = (self.turn + turns_ahead) as usize;
         &mut self.state[turn_index as usize][*planet_index]
     }
 
 
-    fn predict_planets(&self, turns_ahead: u64) -> &Vec<(Option<PlayerId>, i64)> {
-        let turn_index = self.turn + turns_ahead;
+    fn predict_planets(&self, turns_ahead: i64) -> Vec<(Option<PlayerId>, i64)> {
+        // let turn_index = self.turn + turns_ahead;
+        let mut return_vec = Vec::new();
+        for planet_name in &self.planet_names {
+            return_vec.push(
+                self.predict_planet(turns_ahead, &planet_name)
+            );
+        }
+        return_vec
         // &self.state[turn_index as usize]
     }
 
     // TODO: test correctness
-    fn predict_planet(&self, turns_ahead: u64, planet_name: PlanetName) -> (Option<PlayerId>, i64) {
+    fn predict_planet(&self, turns_ahead: i64, planet_name: &PlanetName) -> (Option<PlayerId>, i64) {
+        let planet_name = planet_name.clone();
         let turn_index = (self.turn + turns_ahead) as usize;
         let planet_index = *self.planet_map.get(&planet_name).unwrap();
         let planet_state = self.current_state.planets
             .iter()
             .find(|planet| planet.name == planet_name )
             .unwrap();
-        let mut count = planet_state.ship_count;
-        let mut owner = planet_state.owner;
-        let mut map = BTreeMap::new();
+        let mut current_owner = planet_state.owner;
+        let mut current_count = planet_state.ship_count;
+        let mut map: BTreeMap<Option<u8>, i64> = BTreeMap::new();
         for i in (self.turn as usize)..=turn_index {
             map.clear();
-            map.insert(&owner, count);
+            // if not checking the current state and the owner of the planet is a player, then
+            // ship count will have grown with 1 
+            if (i as i64) != self.turn && current_owner.is_some() {
+                map.insert(current_owner, current_count + 1);
+            } else {
+                map.insert(current_owner, current_count);
+            }
 
-            let deltas = &self.state[turn_index][planet_index].deltas;
+            let deltas = &self.state[i][planet_index].deltas;
             for &(owner, amount) in deltas {
                 let key = Some(owner);
-                map.entry(&key)
-                    .and_modify(|val| *val += amount)
-                    .or_insert(amount);
+                let value = map.get(&key);
+                if planet_name == "protos" && i < 12 {
+                    eprintln!("{} {:?}", i, value);
+                }
+                map.insert(key, value.unwrap_or(&0) + amount);
             }
 
             let key_values = map.iter()
-                .sorted_by_key(|(k, &v)| -v) // sorts by ascending order, so for negative value
+                .sorted_by_key(|(_, &v)| -v) // sorts by ascending order, so for negative value
                 // sorts descending order
                 .take(2)
                 .collect_vec();
 
+
+            if planet_name == "protos" && i == 12 {
+                eprintln!("{}, {:?}",i, key_values);
+            }
+
             let (largest_owner, largest_count) = key_values[0];
             if key_values.len() == 1 {
-                (owner, count) = (**largest_owner, *largest_count);
+                (current_owner, current_count) = (*largest_owner, *largest_count);
             } else {
-                let (next_owner, next_count) = key_values[1];
-                count = largest_count - next_count;
-                if count == 0 {
-                    owner = None;
+                let (_, next_count) = key_values[1];
+                current_count = largest_count - next_count;
+                if current_count == 0 {
+                    current_owner = None;
                 } else {
-                    owner = **largest_owner;
+                    current_owner = *largest_owner;
                 }
             }
         }
 
-        (owner, count)
+        (current_owner, current_count)
     }
 
     fn check_gameover(&self) -> GameSituation {
-        //TODO: look at next turn
-        let ship_counts = self.get_planet_future(1)
+        // TODO: currently only checks units on planets and does not take expeditions into account 
+        let binding = self.predict_planets(1);
+        let ship_counts = binding
             .iter()
-            .into_grouping_map_by(|planet| planet.planet.owner)
-            .fold(0, |acc, _key, val| acc + val.planet.ship_count);
+            .into_grouping_map_by(|(owner, _)| owner)
+            .fold(0, |acc, _key, (_, val)| acc + val);
 
-        eprintln!("{:?}", ship_counts);
-        if ship_counts[&Some(1)] == 0 {
+        let temp = self.current_state.planets
+            .iter()
+            .into_grouping_map_by(|planet| planet.owner)
+            .fold(0, |acc, _key, val| acc + val.ship_count);
+        // eprintln!("current   : {:?}", self.current_state.expeditions);
+        // eprintln!("current   : {:?}", temp);
+        // eprintln!("prediction: {:?}", ship_counts);
+        if !ship_counts.contains_key(&Some(1)) {
             return GameSituation::LOST;
         } 
 
         let other_key = ship_counts.keys()
             .into_iter()
-            .find(|key| key.is_some())
-            .unwrap();
-        if ship_counts[other_key] == 0 {
+            .find(|&&&key| key.is_some() && key != Some(1));
+        if other_key.is_none() {
             return GameSituation::WON;
         }
-        return GameSituation::ONGOING; 
+        GameSituation::ONGOING 
     } 
 
-    fn new(planets: &[Planet]) -> Self {
+    fn new(input: Input) -> Self {
         let mut entry = vec![];
         let mut planet_map = BTreeMap::new();
+        let mut planet_names = vec![];
 
-        for (index, planet) in planets.iter().enumerate() {
+        for (index, planet) in input.planets.iter().enumerate() {
             entry.push(
                 StateCell {
-                    planet: planet.clone(),
                     deltas: vec![],
                 }
             );
 
             planet_map.insert(planet.name.clone(), index);
+            planet_names.push(planet.name.clone());
         }
 
         let mut state_vec = vec![];
@@ -185,6 +215,8 @@ impl State {
             state_vec.push(entry.clone());
         }
         State {
+            planet_names,
+            current_state: input,
             state: state_vec,
             planet_map,
             saved_expeditions: BitSet::new(),
@@ -192,7 +224,7 @@ impl State {
         }
     }
 
-    fn update(&mut self, input: &Input) {
+    fn update(&mut self, input: Input) {
         for expedition in &input.expeditions {
             if self.saved_expeditions.contains(expedition.id as usize) {
                 continue;
@@ -203,7 +235,7 @@ impl State {
                 (expedition.owner, expedition.ship_count as i64)
             );
         }
-        self.state = input;
+        self.current_state = input;
     }
 }
 
@@ -217,6 +249,8 @@ fn main() {
 
     let stdin = io::stdin();
     let mut state = State {
+        current_state: Input { planets: vec![], expeditions: vec![] },
+        planet_names: vec![],
         state: vec![],
         saved_expeditions: BitSet::new(),
         planet_map: BTreeMap::new(),
@@ -225,18 +259,46 @@ fn main() {
 
 
     for line in stdin.lock().lines() {
+
+        if state.turn > 1 {
+            break;
+        }
         let now = Instant::now();
         let line = line.unwrap();
         eprintln!("=========================================================");
         // eprintln!("{}", line);
         let input: Input = serde_json::from_str(&line).unwrap();
-
-        
         if state.turn == 0 {
-            state = State::new(&input.planets);
+            state = State::new(input);
+        } else {
+            state.update(input);
+        }
+        let mut table = Table::new();
+        let mut header = vec![Cell::new("turn")];
+        for planet in &state.planet_names {
+            header.push(Cell::new(&planet.clone()));
+        }
+        table.set_titles(Row::new(header));
+        for i in 0..=12 {
+            let mut predict_row = vec![Cell::new(&(state.turn + i).to_string())];
+            for planet_name in &state.planet_names {
+                let (owner, amount) = state.predict_planet(i, planet_name);
+                let cell = Cell::new(
+                    &format!("{:?}\n{:?}", owner, amount)
+                );
+                predict_row.push(cell);
+            }
+            table.add_row(Row::new(predict_row));
+        }
+        eprintln!("{:?}", state.current_state.expeditions);
+        eprintln!("{}", table);
+
+        match state.check_gameover() {
+            GameSituation::WON => eprintln!("WE HAVE WON!"),
+            GameSituation::LOST => eprintln!("WE HAVE LOST!"),
+            _ => {}
         }
 
-        state.update(&input);
 
         let output = Output {
             moves: vec![]
